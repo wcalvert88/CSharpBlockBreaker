f072b20785de1bf803828a8a4c2d83170ffd5a32
wcalvert88 <wcalvert88@gmail.com>
2018-09-28 20:52:53 -0400

    created a launch ball method to shoot the ball off the paddle

---
+++ Block Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    // config parameters
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            hasStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
