e20afda9cdb46187f5203f071d4efc7460d4b136
wcalvert88 <wcalvert88@gmail.com>
2018-10-02 16:11:31 -0400

    created a playClipAtPoint sound effect so sound doesn't disappear as the blocks break

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Block : MonoBehaviour {
+    [SerializeField] AudioClip breakSound;
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
         Destroy(gameObject);
         Debug.Log(collision.gameObject.name);
     }
