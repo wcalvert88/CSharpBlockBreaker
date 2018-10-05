e6f2d3d4f195d46d49aadd2e85a0b3bdba5822dd
wcalvert88 <wcalvert88@gmail.com>
2018-10-05 10:38:44 -0400

    made the max hits work better and bug free

--- Block Breaker/Assets/Scripts/Block.cs
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -8,7 +8,7 @@ public class Block : MonoBehaviour {
     // config parameters
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits;
+    // [SerializeField] int maxHits;
     [SerializeField] Sprite[] hitSprites;
 
     // cached reference
@@ -39,6 +39,7 @@ public class Block : MonoBehaviour {
 
     private void HandleHit()
     {
+        maxHits = hitSprites.Length + 1;
         if (tag == "Breakable")
         {
             timesHit++;
@@ -56,7 +57,13 @@ public class Block : MonoBehaviour {
     private void ShowNextHitSprite()
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        } else
+        {
+            Debug.LogError("Block sprite is missing from array" + gameObject.name);
+        }
     }
 
     private void DestroyBlock()
